bd008b79753fa552f08fe340b0d08efa02b9a12f
karasique69 <karasiq69@gmail.com>
2024-01-11 07:53:35 +0300

    build: Добавлен скелет проекта

---
+++ impl/src/main/java/com/example/genealogyback/GenealogyBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.genealogyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenealogyBackApplication.class, args);
+    }
+
+}
---
+++ impl/src/test/java/com/example/genealogyback/GenealogyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genealogyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenealogyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
