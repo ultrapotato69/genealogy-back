1b2b51bf359e3c61f147092d3d645b23ee3a332b
karasique69 <karasiq69@gmail.com>
2024-01-27 23:12:16 +0300

    feat(repo): Добавить delete update

--- impl/src/main/java/com/example/genealogyback/repository/PersonRepository.java
+++ impl/src/main/java/com/example/genealogyback/repository/PersonRepository.java
@@ -2,39 +2,15 @@ package com.example.genealogyback.repository;
 
 import com.example.genealogyback.dto.BasePersonDto;
 import com.example.genealogyback.dto.ResponsePersonDto;
-import com.example.genealogyback.exception.NoSuchResourceException;
-import com.example.genealogyback.jooq.tables.daos.PersonsDao;
-import org.jooq.DSLContext;
-import org.jooq.exception.DataAccessException;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-import static com.example.genealogyback.jooq.tables.Persons.PERSONS;
+public interface PersonRepository {
+    ResponsePersonDto insert(BasePersonDto personDto);
 
-@Repository
-public class PersonRepository extends PersonsDao {
-    private final DSLContext dsl;
+    ResponsePersonDto readById(UUID id);
 
-    public PersonRepository(DSLContext dsl) {
-        this.dsl = dsl;
-    }
+    ResponsePersonDto update(UUID id, BasePersonDto basePersonDto);
 
-    public ResponsePersonDto insert(BasePersonDto personDto) {
-        return dsl.insertInto(PERSONS)
-                .set(dsl.newRecord(PERSONS, personDto))
-                .returning()
-                .fetchOptional()
-                .orElseThrow(() -> new DataAccessException("Error inserting entity: [%s].".formatted(personDto)))
-                .into(ResponsePersonDto.class);
-    }
-
-    public ResponsePersonDto readById(UUID id) {
-        return dsl.selectFrom(PERSONS)
-                .where(PERSONS.ID.eq(id))
-                .fetchOptional()
-                .orElseThrow(() -> new NoSuchResourceException("Person with [%s] is not found."
-                        .formatted(id)))
-                .into(ResponsePersonDto.class);
-    }
+    void deleteById(UUID id);
 }
