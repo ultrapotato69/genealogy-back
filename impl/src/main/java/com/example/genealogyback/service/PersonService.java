a5ff2da4e2feb3d95423910708892dbaa2b1c939
karasique69 <karasiq69@gmail.com>
2024-02-18 00:44:58 +0300

    feat(serv): Добавление супруга, родителей и детей
    Implements: issues/16

--- impl/src/main/java/com/example/genealogyback/service/PersonService.java
+++ impl/src/main/java/com/example/genealogyback/service/PersonService.java
@@ -1,6 +1,7 @@
 package com.example.genealogyback.service;
 
 import com.example.genealogyback.dto.BasePersonDto;
+import com.example.genealogyback.dto.PersonWithRelativesDto;
 import com.example.genealogyback.dto.ResponsePersonDto;
 
 import java.util.UUID;
@@ -8,7 +9,7 @@ import java.util.UUID;
 public interface PersonService {
     ResponsePersonDto create(BasePersonDto personDto);
 
-    ResponsePersonDto readById(UUID id);
+    PersonWithRelativesDto readById(UUID id);
 
     ResponsePersonDto update(UUID id, BasePersonDto basePersonDto);
 
